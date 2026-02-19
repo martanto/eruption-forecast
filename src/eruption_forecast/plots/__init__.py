2ca04440a6d67acf32a09d4329dccee236cdd305
Martanto <martanto@live.com>
2026-02-19 16:52:56 +0100

    Aggregation metrics plot

--- src/eruption_forecast/plots/__init__.py
+++ src/eruption_forecast/plots/__init__.py
@@ -47,6 +47,18 @@ from eruption_forecast.plots.evaluation_plots import (
     plot_prediction_distribution,
 )
 
+# Aggregate evaluation plotting
+from eruption_forecast.plots.aggregate_evaluation_plots import (
+    plot_aggregate_roc,
+    plot_all_aggregate,
+    plot_aggregate_calibration,
+    plot_aggregate_confusion_matrix,
+    plot_aggregate_precision_recall,
+    plot_aggregate_feature_importance,
+    plot_aggregate_threshold_analysis,
+    plot_aggregate_prediction_distribution,
+)
+
 
 __all__ = [
     # Style configuration
@@ -70,6 +82,15 @@ __all__ = [
     "plot_feature_importance",
     "plot_calibration",
     "plot_prediction_distribution",
+    # Aggregate evaluation plotting
+    "plot_aggregate_roc",
+    "plot_aggregate_precision_recall",
+    "plot_aggregate_calibration",
+    "plot_aggregate_prediction_distribution",
+    "plot_aggregate_confusion_matrix",
+    "plot_aggregate_threshold_analysis",
+    "plot_aggregate_feature_importance",
+    "plot_all_aggregate",
     # Forecast plotting
     "plot_forecast",
     "plot_forecast_with_events",
